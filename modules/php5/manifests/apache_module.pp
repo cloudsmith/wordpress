711c08d5d7ec594d936ae03b9c4d7b5a4940b679
mruzicka <michal.ruzicka@cloudsmith.com>
2013-03-29 20:15:20 +0100

    use fastcgi as the mechanism to run php from apache on ubuntu

--- modules/php5/manifests/apache_module.pp
+++ modules/php5/manifests/apache_module.pp
@@ -3,9 +3,9 @@ class php5::apache_module {
 	include apache2
 
 	$php = $::operatingsystem ? {
-		Ubuntu => 'libapache2-mod-php5',
+		Ubuntu => 'php5-fpm',
 		CentOS => 'php',
-		Debian => 'libapache2-mod-php5',
+		Debian => 'php5-fpm',
 		default => 'php',
 	}
 
@@ -16,5 +16,49 @@ class php5::apache_module {
 		notify => Service['apache2-service'],
 	}
 
-	Service['apache2-service'] <~ Php5::Plugin<| |>
+	case $::operatingsystem {
+		'debian', 'ubuntu': {
+			$php_handler = 'php5-fcgi'
+
+			file { '/etc/apache2/mods-enabled/php5-fpm-fcgi.conf':
+				ensure => present,
+				source => 'puppet:///modules/php5/php5-fpm-fcgi.conf',
+				owner => 'root',
+				group => 'root',
+				mode => '0644',
+				require => Package['apache2'],
+				notify => Service['apache2-service'],
+			}
+
+			exec { 'add-multiverse-repo':
+				command => 'perl -ne \'m|^#\s*(deb(?:-src)?\s+http://.*\s+multiverse\n)|s and print $1\' /etc/apt/sources.list > /etc/apt/sources.list.d/multiverse.list; apt-get update',
+				path => ['/usr/local/bin', '/bin', '/usr/bin'],
+				creates => '/etc/apt/sources.list.d/multiverse.list',
+			} -> Package<| |>
+
+			package { 'libapache2-mod-fastcgi':
+				ensure => latest,
+				require => Package['apache2'],
+				notify => Service['apache2-service'],
+			}
+
+			apache2::module { ['alias', 'actions', 'fastcgi']:
+				ensure => 'enabled',
+			}
+
+			service { 'php5-fpm':
+				ensure => running,
+				enable => true,
+				hasrestart => true,
+				hasstatus => true,
+				require => Package['php5-apache_module'],
+			} <~ Php5::Plugin<| |>
+		}
+
+		default: {
+			$php_handler = 'php5-script'
+
+			Service['apache2-service'] <~ Php5::Plugin<| |>
+		}
+	}
 }
